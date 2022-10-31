28003ef1bee10bc565801f8853b34e201c230169
NicoloCasagrande <nicolo.casagrande7@gmail.com>
2022-10-31 19:55:23 +0100

    finish

--- paridispari/script.js
+++ paridispari/script.js
@@ -10,7 +10,6 @@ function pariDispari(word){
     while(correct === false){
         if(word === "pari" || word === "dispari"){
             correct = true;
-            console.log(word);
         }else{
             word = prompt("inserisci solamente o pari o dispari !");
         }
@@ -37,6 +36,42 @@ while(isNaN(userNumber)){
     userNumber = Number(prompt("inserisci un numero da 1 a 5"));
 }
 
+console.log(userChoice);
 console.log(userNumber);
+
+
+function randomOneFive(num1, num2){
+    return Math.floor(Math.random() * (num2 - num1 + 1) + num1);
+}
+
+cpuNumber = randomOneFive(1,5);
+
+console.log(cpuNumber);
+
+sum = sumOddEase(cpuNumber, userNumber);
+
+function sumOddEase(num1, num2){
+    return num1 + num2;
+}
+
+console.log(sum);
+
+function isEven(num){
+
+    if(num % 2 === 0){
+        return true;
+    }
+    return false;
+}
+
+if(isEven(sum) === true && userChoice === "pari"){
+    console.log("hai vinto");
+}else if(isEven(sum) === false && userChoice === "dispari"){
+    console.log("hai vinto");
+}else{
+    console.log("hai perso");
+}
+
+
 
 
